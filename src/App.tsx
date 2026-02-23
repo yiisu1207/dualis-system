078996e1cd5e6921d8b6e90fe405299b1a317cad
Jxsuu1207 <jesusboutique42@gmail.com>
2026-02-23 17:18:15 -0400

    FORCED: Seguridad triple, PIN maestro y sugerencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// Deploy Version: 1.0.1 - PIN & SECURITY
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
